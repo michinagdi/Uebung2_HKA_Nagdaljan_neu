3526abf2adbfcad821636e8f6b6ce59a5aaebc81
michinagdi <m.nagdaljan@gmail.com>
2023-10-25 00:55:36 +0200

    Initial commit

---
+++ app/src/main/java/de/augsburgermichi/hka/uebung2_hka_nagdaljan/Activity_Weiter.java
@@ -0,0 +1,23 @@
+package de.augsburgermichi.hka.uebung2_hka_nagdaljan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Activity_Weiter extends AppCompatActivity {
+
+    private TextView textViewWeiter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weiter);
+
+        textViewWeiter = this.findViewById(R.id.txtMessageWeiter);
+        textViewWeiter.setText(MainActivity.getTextViewMiddle().getText());
+    }
+
+    public void penis() {
+
+    }
+}
---
+++ app/src/main/java/de/augsburgermichi/hka/uebung2_hka_nagdaljan/MainActivity.java
@@ -0,0 +1,52 @@
+package de.augsburgermichi.hka.uebung2_hka_nagdaljan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button_click;
+    private Button button_weiter;
+    private static TextView textViewMiddle;
+    private String[] listOfRandomWords = new String[6];
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World!");
+
+        button_click = this.findViewById(R.id.btn_click);
+        button_weiter = this.findViewById(R.id.btn_next);
+        textViewMiddle = this.findViewById(R.id.txtMessage);
+
+        listOfRandomWords[0] = "Heute ist ein schöner Tag!";
+        listOfRandomWords[1] = "1+1=3";
+        listOfRandomWords[2] = "Tomaten sind lecker";
+        listOfRandomWords[3] = "Augsburg ist die beste Stadt";
+        listOfRandomWords[4] = "Ich mag Züge";
+        listOfRandomWords[5] = "Miau :3";
+
+        button_click.setOnClickListener(view -> {
+            Random r = new Random();
+            textViewMiddle.setText(listOfRandomWords[r.nextInt(6)]);
+        });
+
+        button_weiter.setOnClickListener(view -> {
+            Intent activity_weiter = new Intent(this, Activity_Weiter.class);
+            this.startActivity(activity_weiter);
+        });
+
+    }
+
+    public static TextView getTextViewMiddle() {
+        return textViewMiddle;
+    }
+}
